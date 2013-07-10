70713de9afc01942a4a33a079dd5021c27e1fc76
Rodrigo Alvarez <rastmails@gmail.com>
2013-07-10 01:03:15 +0000

    changed web.js_v11

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require('express');
 var fs      = require('fs');
 var buf     = new Buffer(256); 
 var app     = express.createServer(express.logger());
-var content = fs.readFileSync('index.html','utf-8')
+var content = fs.readFileSync("index.html");
+var index   = buf.toString('utf-8',content);
 app.get('/', function(request,response){
-    response.send(buf.toString('utf-8',content))
+    response.send(index);
     if(err) throw err;
     console.log('It\'s done!');
 });
