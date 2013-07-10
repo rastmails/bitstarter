f416e58c511fc3da5d610a690de8b58f1678f826
Rodrigo Alvarez <rastmails@gmail.com>
2013-07-10 00:19:06 +0000

    Changed web.js_v3

--- web.js
+++ web.js
@@ -1,8 +1,14 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var express = require('express');
 
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(index);
-}).listen(9615);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request,response){
+    response.render('index.html');
+    if(err) throw err;
+    console.log('It\'s done!');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function(){
+    console.log("Listening on " + port);
+});
