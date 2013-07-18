03186a790f7f17cc26bd4b033a364f04e4b6df19
Rodrigo Alvarez <rastmails@gmail.com>
2013-07-18 00:40:55 +0000

    Modified web.js to listen on port 8080

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var fs      = require('fs');
 var app     = express.createServer(express.logger());
-var content = fs.readFileSync("index.html");
-var index   = content.toString();
+var htmlfile = "index.html";
+
+
 app.get('/', function(request,response){
-    response.send(index);
-    if(err) throw err;
-    console.log('It\'s done!');
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function(){
     console.log("Listening on " + port);
 });
